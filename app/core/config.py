b032c807f87570c598493b6aeb989e5794f01418
Araf Mahmud <emtiaz@Arafs-Mac-Mini.local>
2026-03-13 12:53:46 +0600

    Sqlite3

--- app/core/config.py
+++ app/core/config.py
@@ -3,3 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+class Config:
+    def __init__(self):
+        self.URL = "https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sql"
+ 
+    
--- app/db/sqlite3_client.py
+++ app/db/sqlite3_client.py
@@ -1,3 +1,26 @@
 import sqlite3
 import requests
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
+
+from app.core.config import Config
+
+config = Config()
+
+def get_engine_chinook_db():
+     url = Config().URL
+     response =  requests.get(url)
+     sql_script = response.text
+
+     connection = sqlite3.connect(':memory:', check_same_thread=False)
+
+     connection.executescript(sql_script)
+
+     return create_engine(
+          "sqlite://",
+          creator = lambda : connection,
+          poolclass = StaticPool,
+          conncection_args = {"check_same_thread": False}
+     )
+
 
