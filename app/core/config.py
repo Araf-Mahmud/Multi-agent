43a5dedc1435abd33eab3276ada6d902251eeef5
Araf Mahmud <arafmahmud@Arafs-MacBook-Air.local>
2026-03-14 19:49:35 +0600

    Working on Agent 1

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,22 @@
 import os
 from dotenv import load_dotenv
 
+from langchain_groq import ChatGroq
+
 load_dotenv()
 
 class Config:
     def __init__(self):
         self.URL = "https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sql"
- 
+    
+        self.GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+        
+    def get_oss_llm(self) -> ChatGroq:
+        llm = ChatGroq(
+            api_key = self.GROQ_API_KEY, 
+            model = 'openai/gpt-oss-120b', 
+            temperature = 0.4
+        )
+
+        return llm
     
---
+++ app/schemas/customer_suppprt_schema.py
@@ -0,0 +1,5 @@
+from typing import Annotated, List
+from typing_extensions import TypedDict
+from langgraph.managed.is_last_step import RemainingSteps
+from langgraph.graph.message import AnyMessage, add_messages
+
